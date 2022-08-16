c0db37b0004b92c17d4b1e2af7e1dd7e26178161
AlphaGhostUSMC <Soulofninja69@gmail.com>
2022-08-16 07:19:22 -0700

    Removed old files

--- problems/charDataType.c
+++ problems/charDataType.c
@@ -1,4 +1,4 @@
-// A Simple c program to demonstrate the use of char data type.
+// example program using the char data type
 
 #include <stdio.h>
 
@@ -7,11 +7,6 @@ int main()
   char ch;
   printf("Enter a character: ");
   scanf("%c", &ch);
-  printf("The ASCII value of %c is %d\n", ch, ch);
+  printf("You entered: %c\n", ch);
   return 0;
 }
-// Output:
-// Enter a character: a
-// The ASCII value of a is 97
-// Enter a character: A
-// The ASCII value of A is 65
