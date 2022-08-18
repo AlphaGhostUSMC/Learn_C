3d944b6f05fefff7f172bd1b78b74008b43bf878
AlphaGhost <Soulofninja69@gmail.com>
2022-08-18 12:19:16 +0530

    Nil

---
+++ problems/P3.c
@@ -0,0 +1,15 @@
+// Program 10
+
+#include <stdio.h>
+
+int main()
+{
+  int a, b, c, d;
+  printf("Enter two numbers: ");
+  scanf("%d %d", &a, &b);
+  c = a + b;
+  d = a - b;
+  printf("%d", c);
+  printf("\n%d", d);
+  return 0;
+}
